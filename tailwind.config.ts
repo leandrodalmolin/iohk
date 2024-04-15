63129938e1787d716a53abb024049a804d134684
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2024-04-15 07:01:47 +0100

    Extend theme with custom tokens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,25 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lavender: {
+          100: '#ead0ff',
+          200: '#dfbffa',
+        },
+        peach: {
+          100: '#ffe9d5',
+          200: '#f9ddc2',
+        },
+      },
+      backgroundImage: {
+        'brand-gradient':
+          'linear-gradient(101.69deg, #e5e9fa 25.94%, #f6dede 102.42%)',
+      },
+      boxShadow: {
+        default: '0px 4px 25px 0px rgba(0, 0, 0, 0.15)',
+      },
+    },
   },
   plugins: [],
 }
