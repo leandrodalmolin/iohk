55d8c7c073b448349d8cd38a8f4eadb03977f208
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2024-04-15 10:08:08 +0100

    Add placeholder blur to hero image

--- src/components/hero/image.tsx
+++ src/components/hero/image.tsx
@@ -4,9 +4,10 @@ import { HeroImageProps } from './hero.types'
 export function HeroImage({ image }: HeroImageProps) {
   return (
     <Image
-      className="motion-safe:animate-fade-in motion-safe:md:animate-fade-in-to-left w-full rounded-2xl md:absolute md:top-0 md:h-full md:w-[50vw] md:rounded-l-2xl md:object-cover"
+      className="w-full rounded-2xl motion-safe:animate-fade-in md:absolute md:top-0 md:h-full md:w-[50vw] md:rounded-l-2xl md:object-cover motion-safe:md:animate-fade-in-to-left"
       src={image.src}
       alt={image.alt}
+      placeholder="blur"
       priority
     />
   )
